63d262f35a56ab7a01df121f1b98342a90d2257e
Behnam Banaei <behban20@gmail.com>
2022-08-25 11:00:48 +0430

    added viewModel data collecting

--- app/src/main/java/ir/bitokss/bitokapp/presentation/events/EventsFragment.kt
+++ app/src/main/java/ir/bitokss/bitokapp/presentation/events/EventsFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import ir.bitokss.bitokapp.common.utils.CustomResponse
+import ir.bitokss.bitokapp.common.utils.CustomResponse.Status.*
 import ir.bitokss.bitokapp.databinding.FragmentEventsBinding
 
 @AndroidEntryPoint
 class EventsFragment : Fragment() {
 
     private lateinit var binding: FragmentEventsBinding
+    private val viewModel by viewModels<EventsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +25,28 @@ class EventsFragment : Fragment() {
     ): View {
         binding = FragmentEventsBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initCollectors()
+        viewModel.getAllEvents()
+    }
+
+    private fun initCollectors() {
+        lifecycleScope.launchWhenResumed {
+            viewModel.events.collect {
+                when (it?.status) {
+                    SUCCESS -> TODO()
+                    FAIL -> TODO()
+                    ERROR -> TODO()
+                    LOADING -> TODO()
+                    null -> TODO()
+                }
+            }
+        }
+
     }
 
 
--- app/src/main/java/ir/bitokss/bitokapp/presentation/events/EventsViewModel.kt
+++ app/src/main/java/ir/bitokss/bitokapp/presentation/events/EventsViewModel.kt
@@ -1,14 +1,32 @@
 package ir.bitokss.bitokapp.presentation.events
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.bitokss.bitokapp.common.utils.CustomResponse
+import ir.bitokss.bitokapp.data.remote.dto.Event
 import ir.bitokss.bitokapp.data.repository.EventRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class EventsViewModel @Inject constructor(
-    private val repository: EventRepository
+    private val eventRepository: EventRepository
 ) : ViewModel() {
 
+    private val _events: MutableStateFlow<CustomResponse<List<Event>>?> = MutableStateFlow(null)
+    val events = _events.asStateFlow()
 
+    fun getAllEvents() {
+        viewModelScope.launch {
+            eventRepository.getAllEvents("").collect {
+                _events.value = it
+            }
+        }
+    }
 }
