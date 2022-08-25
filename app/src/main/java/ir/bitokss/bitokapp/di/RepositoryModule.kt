393fa8372d1cdf6a9a80b657bb7333a2ce57ba0f
Behnam Banaei <behban20@gmail.com>
2022-08-25 11:53:04 +0430

    added fake repository added sample ui data handling

--- app/src/main/java/ir/bitokss/bitokapp/di/RepositoryModule.kt
+++ app/src/main/java/ir/bitokss/bitokapp/di/RepositoryModule.kt
@@ -7,13 +7,18 @@ import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 import ir.bitokss.bitokapp.data.repository.EventRepository
 import ir.bitokss.bitokapp.data.repository.EventRepositoryImpl
+import ir.bitokss.bitokapp.data.repository.FakeEventRepositoryImpl
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class RepositoryModule {
 
+    //todo replace backend is ready
+//    @Provides
+//    @ViewModelScoped
+//    fun provideEventRepository(impl: EventRepositoryImpl): EventRepository = impl
+
     @Provides
     @ViewModelScoped
-    fun provideEventRepository(impl: EventRepositoryImpl): EventRepository = impl
-
+    fun provideEventRepository(impl: FakeEventRepositoryImpl): EventRepository = impl
 }
---
+++ app/src/main/java/ir/bitokss/bitokapp/presentation/events/EventView.kt
@@ -0,0 +1,7 @@
+package ir.bitokss.bitokapp.presentation.events
+
+data class EventView(
+    val id: Int,
+    val title: String,
+) {
+}
